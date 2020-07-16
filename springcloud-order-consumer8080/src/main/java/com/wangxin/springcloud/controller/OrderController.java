dc53b34914b94eb2c5cb9822f61e6ac22e82259b
wangxin12030726 <18910259163.com>
2020-07-16 12:19:32 +0800

    springboot-actuator的添加

--- springcloud-order-consumer8080/src/main/java/com/wangxin/springcloud/controller/OrderController.java
+++ springcloud-order-consumer8080/src/main/java/com/wangxin/springcloud/controller/OrderController.java
@@ -1,10 +1,15 @@
 package com.wangxin.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.wangxin.springcloud.entities.Account;
 import com.wangxin.springcloud.entities.CommonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -14,9 +19,16 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
-
+    @HystrixCommand(fallbackMethod = "defaultFindAll")
     @RequestMapping("/order/findAll")
     public CommonResult findAll(){
         return restTemplate.getForObject(PRIMARY_URL+"/account/findAll",CommonResult.class);
+    }
+
+    public CommonResult defaultFindAll(){
+        Account account = new Account(-1,"test",200);
+        List<Account> list = new ArrayList<Account>();
+        list.add(account);
+        return new CommonResult<List>(500,"查询失败，请稍后再试",list);
     }
 }
